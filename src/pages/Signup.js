d788a89d395b2692cdf675a8844d679df08e85f8
Mohammed Alsahli <xmxm.717@gmail.com>
2022-12-10 16:34:09 +0300

    signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styling/App.css";
 import Navbar from "../components/Navbar";
 import Intro from "../components/Intro";
-import Signup from "../components/signUp"
-
-
+import Signup from "../components/signUp";
+import { Navigate } from "react-router-dom";
+import { auth } from "../components/fb";
 
 function App() {
-  const text = "Signup";
-  return (
-    <div className="App">
-      <Navbar />
-      <Intro text={text} />
-      <Signup />
-    </div>
-  );
+  let [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, [user]);
+  if (!user) {
+    const text = "Signup";
+    return (
+      <div className="App">
+        <Navbar />
+        <Intro text={text} />
+        <Signup />
+      </div>
+    );
+  }
+  return <Navigate to="/"></Navigate>;
 }
 
 export default App;
